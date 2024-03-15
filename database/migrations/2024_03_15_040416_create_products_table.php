0802755977406ee12ba45591c6a5cd1a0ca1f8a5
NaylaFarras <yanbintang8@gmail.com>
2024-03-15 11:13:14 +0700

    1

---
+++ database/migrations/2024_03_15_040416_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categorys');
+            $table->foreignId('shop_id')->constrained('shops');
+            $table->foreignId('order_id')->constrained('orders');
+            $table->string('image')->nullable();
+            $table->string('nama', 255);
+            $table->text('deskripsi');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
